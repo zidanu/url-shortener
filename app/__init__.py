a2ae12a309133afd7612e5ad24796a0b7cd34485
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-04-03 21:33:39 -0400

    feat: return json errors

--- app/__init__.py
+++ app/__init__.py
@@ -20,4 +20,12 @@ def create_app():
     def health():
         return jsonify(status="ok")
 
+    @app.errorhandler(404)
+    def not_found(e):
+        return jsonify({"error": "Not found"}), 404
+
+    @app.errorhandler(500)
+    def server_error(e):
+        return jsonify({"error": "Internal server error"}), 500
+
     return app
